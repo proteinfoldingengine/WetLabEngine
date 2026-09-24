c9d4a4a7652d286895fcbe62540caf36b2178456
proteinfoldingengine <aspradaz@gmail.com>
2026-09-24 08:00:37 -0700

    test(uqcf): fix v15.53 transitivity test reference

--- ResearchHistory/UQCF-GEM/demos/v15.53-lawful-quantum-realization-reduction/test_equivalence.py
+++ ResearchHistory/UQCF-GEM/demos/v15.53-lawful-quantum-realization-reduction/test_equivalence.py
@@ -67,7 +67,7 @@ class EquivalenceTests(unittest.TestCase):
     def test_symmetry_and_transitivity(self):
         c4r2=relabel(self.c4,(2,3,0,1),(1,3,0,2))
         self.assertTrue(eq.are_equivalent(self.c,self.c4r,self.c4))
-        self.assertTrue(eq.are_equivalent(self.c,self.c4,self.c4r2))
+        self.assertTrue(eq.are_equivalent(self.c,self.c4,c4r2))
         self.assertTrue(eq.are_equivalent(self.c,self.c4r,self.c4r2))
 
     def test_id_swap_does_not_change_class(self):
